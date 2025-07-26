fa5ee6909f7ba2d9e61c7179b639c6bf9c53c269
Sombrechip88244 <166306750+Sombrechip88244@users.noreply.github.com>
2025-07-26 20:06:36 +0100

    Update shell-switcher.rb

--- Formula/shell-switcher.rb
+++ Formula/shell-switcher.rb
@@ -1,7 +1,7 @@
 class ShellSwitcher < Formula
   desc "A simple shell switcher written in a shell script"
   homepage "https://github.com/Sombrechip88244/SS---Shell-Switcher"
-  url "https://github.com/Sombrechip88244/SS---Shell-Switcher/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/Sombrechip88244/SS---Shell-Switcher/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "GPL-3.0"
 
