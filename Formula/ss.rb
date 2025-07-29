1dd5890e3beb8fa0e7548a4e9f1b377ef579ea9f
Oliver Fildes <Oliver.fildes@proton.me>
2025-07-29 08:58:45 +0100

    new name

---
+++ Formula/ss.rb
@@ -0,0 +1,24 @@
+class ShellSwitcher < Formula
+  desc "A simple shell switcher written in a shell script"
+  homepage "https://github.com/Sombrechip88244/ss"
+  url "https://github.com/Sombrechip88244/ss/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "d4fc8bae7b76f4148da916bdf6fbccb320521fd3affb78efd708dbb9ac788674"
+  license "GPL-3.0"
+
+  def install
+    bin.install "main.sh" => "ss"
+    man1.install "man/shell-switcher.1"
+    # Make the script executable
+    chmod 0755, bin/"ss"
+  end
+
+  test do
+    # Test that the script exists and is executable
+    assert_predicate bin/"ss", :exist?
+    assert_predicate bin/"ss", :executable?
+    
+    # Basic functionality test - check if it shows current shell
+    output = shell_output("#{bin}/ss <<< 'no'")
+    assert_match "Your current shell is", output
+  end
+end
